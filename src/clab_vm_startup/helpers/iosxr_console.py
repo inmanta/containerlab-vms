0fc749d14a821848bc07acbf72961e03683b8562
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2021-08-17 13:30:42 +0200

    Added docstring to xrv console class

--- src/clab_vm_startup/helpers/iosxr_console.py
+++ src/clab_vm_startup/helpers/iosxr_console.py
@@ -23,7 +23,16 @@ LOGGER = logging.getLogger(__name__)
 
 
 class IOSXRConsole:
+    """
+    Helper class for interacting with the IOSXR console.  This requires an open telnet connection.
+    """
     def __init__(self, serial_console: TelnetClient, username: str, password: str, cli_prefix: str) -> None:
+        """
+        :param serial_console: The telenet connection already open to the serial console
+        :param username: The username to use to connect to the console
+        :param password: The password to use to connect to the console
+        :param cli_prefix: The cli profix that is expected to be show when we are connected
+        """
         self.username = username
         self.password = password
 
@@ -43,17 +52,34 @@ class IOSXRConsole:
         return f"{self._cli_prefix}:{self._hostname}#"
 
     def connect(self) -> None:
+        """
+        Connect to the IOS console, using the username and password passed to the constructor
+        """
         if self.connected:
             raise RuntimeError("This IOS XR console is already connected")
 
+        username_prompt = re.compile("Username:")
+        cli_prompt = re.compile(r"{prefix}:([^\s]+)#".format(prefix=self._cli_prefix))
+        auth_failed = re.compile("% User Authentication failed")
+
         # Triggering login prompt
         self.wait_write("", None)
-        self.wait_write(self.username, "Username:")
-        self.wait_write(self.password, "Password:")
 
-        auth_failed = re.compile("% User Authentication failed")
-        cli_prompt = re.compile(r"{prefix}:([^\s]+)#".format(prefix=self._cli_prefix))
-        pattern, match, res = self._console.expect(
+        pattern, match, _ = self._console.expect(
+            [username_prompt, cli_prompt],
+            timeout=10,
+        )
+        if pattern == cli_prompt:
+            LOGGER.info("The IOS console was already open")
+            self._hostname = match.group(1)
+            self._connected = True
+            return
+
+        if pattern == username_prompt:
+            self.wait_write(self.username, None)
+            self.wait_write(self.password, "Password:")
+        
+        pattern, match, _ = self._console.expect(
             [auth_failed, cli_prompt],
             timeout=10,
         )
@@ -65,6 +91,9 @@ class IOSXRConsole:
             self._connected = True
 
     def disconnect(self) -> None:
+        """
+        Disconnect from the IOS console
+        """
         if not self.connected:
             raise RuntimeError("This IOS XR console is not yet connected")
 
@@ -78,12 +107,27 @@ class IOSXRConsole:
             self._connected = False
 
     def wait_write(self, write: str, wait: Optional[str] = None, timeout: int = 10) -> None:
+        """
+        Wait for a string to appear to write another string.
+
+        :param write: The string to write
+        :param wait: The string to wait for, if none is provided we don't wait before writing
+        :param timeout: The maximum time we can wait before writing, if this time is exceeded,
+            a TimeoutError is raised.
+        """
         if wait is not None:
             self._console.read_until(wait, timeout)
 
         self._console.write(f"{write}\r")
 
     def generate_rsa_key(self) -> None:
+        """
+        If you are connected to the console, you can call this to setup the ssh rsa keys on the router.
+        If no key is configured a new one of 2048 bits is created, otherwise the existing one is kept.
+        """
+        if not self.connected:
+            raise RuntimeError("You should be connected to the console to setup the rsa key")
+
         LOGGER.info("Configuring rsa key")
         self.wait_write("")
         self.wait_write("terminal length 0", self.cli_prompt)
@@ -92,7 +136,7 @@ class IOSXRConsole:
         # check if we are prompted to overwrite current keys
         new_key = re.compile("How many bits in the modulus")
         key_exists = re.compile("Do you really want to replace them")
-        pattern, match, res = self._console.expect(
+        pattern, _, _ = self._console.expect(
             [
                 new_key,
                 key_exists,
@@ -100,10 +144,11 @@ class IOSXRConsole:
             ],
             10,
         )
-        if match:  # got a match!
-            if pattern == new_key:
-                self.wait_write("2048", None)
-                LOGGER.info("Rsa key configured")
-            elif pattern == key_exists:
-                self.wait_write("no", None)
-                LOGGER.info("Rsa key was already configured")
+        if pattern == new_key:
+            self.wait_write("2048", None)
+            LOGGER.info("Rsa key configured")
+
+        elif pattern == key_exists:
+            self.wait_write("no", None)
+            LOGGER.info("Rsa key was already configured")
+
